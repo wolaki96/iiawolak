e9fd9e9c0607636813afada146cfb158074a9c1d
Squiddy <wolaki96@gmail.com>
2023-02-09 20:06:09 -0500

    changed color

--- src/components/pages/footer.js
+++ src/components/pages/footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import Box from '@mui/material/Box';
